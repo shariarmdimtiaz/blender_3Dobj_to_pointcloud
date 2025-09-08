e5b5dc1b51ce3bcc2e7bb22dfcc0fd3501506bab
shariarmdimtiaz <shariar.md.imtiaz@gmail.com>
2025-09-08 15:50:26 +0900

    implement main part and save the pointcloud data

--- blender_to_pointcloud.py
+++ blender_to_pointcloud.py
@@ -54,3 +54,48 @@ def sample_texture(image, uv):
     return tuple(int(c * 255) for c in color)
 
 
+# Get the selected mesh object
+obj = bpy.context.object
+
+if obj and obj.type == 'MESH':
+    bm = bmesh.new()
+    bm.from_mesh(obj.data)
+    bm.verts.ensure_lookup_table()
+    
+    density = 10000 # Point density
+    point_cloud = []
+    world_matrix = obj.matrix_world
+    image = get_material_texture(obj)
+    uv_layer = obj.data.uv_layers.active
+
+    if not uv_layer:
+        print("No active UV map found!")
+    else:
+        for face in bm.faces:
+            if len(face.verts) < 3:
+                continue
+            
+            uv1 = uv_layer.data[face.loops[0].index].uv
+            uv2 = uv_layer.data[face.loops[1].index].uv
+            uv3 = uv_layer.data[face.loops[2].index].uv
+            v1, v2, v3 = [world_matrix @ v.co for v in face.verts[:3]]
+            sampled_points, uvs = random_points_in_triangle_3d(v1, v2, v3, uv1, uv2, uv3, density)
+            
+            # Get UV coordinates from face
+            
+            
+            for p, uv in zip(sampled_points, uvs):
+                r, g, b = sample_texture(image, uv)       # UV → RGB
+                point_cloud.append((*p, r, g, b))         # XYZ + RGB
+    
+    bm.free()
+
+    output_dir = "E:/projects/Blender 3DModel/point_data/room_5/table_1.txt"
+    output_path = bpy.path.abspath(output_dir)  
+    with open(output_path, "w") as f:
+        for x, y, z, r, g, b in point_cloud:
+            f.write(f"{x} {y} {z} {r} {g} {b}\n")
+
+    print(f"Point cloud data saved!")
+else:
+    print("No mesh object selected!")
